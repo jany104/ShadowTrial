6d9a6a1fb1887d8906a05f89a1410a65b4ea5b48
devikashilu <devikashilu@gmail.com>
2026-01-16 18:17:37 +0530

    app

--- shadowtrial/src/App.jsx
+++ shadowtrial/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { CaseProvider, useCase } from './context/CaseContext'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import IncidentInput from './pages/IncidentInput'
+import QuestionSim from './pages/QuestionSim'
+import EvidenceResult from './pages/EvidenceResult'
+import LegalFlow from './pages/LegalFlow'
+import ChatBot from "./components/ChatBot";
+
+
+const AppContent = () => {
+    const { caseData, nextStep } = useCase()
+
+    const renderPage = () => {
+        switch (caseData.currentStep) {
+            case 'home': return <Home />
+            case 'input': return <IncidentInput />
+            case 'sim': return <QuestionSim />
+            case 'result': return <EvidenceResult />
+            case 'legal': return <LegalFlow />
+            default: return <Home />
+        }
+    }
+
+    return (
+        <div className="app-container">
+            <Navbar />
+            <main>
+                {renderPage()}
+            </main>
+            <Footer />
+        </div>
+    )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <CaseProvider>
+            <AppContent />
+        </CaseProvider>
+    )
 }
 
 export default App
